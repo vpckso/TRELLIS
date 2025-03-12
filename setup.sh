fcfc18fea74d205de1741730504b20b7f483e00d
Kit So <kit.so@visual-paradigm.com>
2025-03-12 14:45:12 +0800

    Support pytorch 2.5.1

--- setup.sh
+++ setup.sh
@@ -144,7 +144,7 @@ if [ "$XFORMERS" = true ] ; then
             case $PYTORCH_VERSION in
                 2.5.0) pip install xformers==0.0.28.post2 --index-url https://download.pytorch.org/whl/cu124 ;;
                 2.5.1) pip install xformers==0.0.28.post2 --index-url https://download.pytorch.org/whl/cu124 ;;
-                2.5.1\+cu124) pip install xformers==0.0.28.post2 --index-url https://download.pytorch.org/whl/cu124 ;;
+                2.5.1\+cu124) pip install xformers==0.0.28.post3 --index-url https://download.pytorch.org/whl/cu124 ;;
                 *) echo "[XFORMERS] Unsupported PyTorch & CUDA version: $PYTORCH_VERSION & $CUDA_VERSION" ;;
             esac
         else
